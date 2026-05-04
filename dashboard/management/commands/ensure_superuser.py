48d84f2700e106297bac43cb4eb5c72de6191053
adilsqe13 <adilsqe13@gmail.com>
2026-05-04 15:53:31 +0530

    Add ensure_superuser management command

---
+++ dashboard/management/commands/ensure_superuser.py
@@ -0,0 +1,31 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Create or update a superuser from env vars (idempotent)."
+
+    def handle(self, *args, **options):
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        if not username or not password:
+            self.stdout.write("Skipping superuser: DJANGO_SUPERUSER_USERNAME/PASSWORD not set.")
+            return
+
+        User = get_user_model()
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={"email": email, "is_staff": True, "is_superuser": True},
+        )
+        user.email = email or user.email
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save()
+
+        action = "Created" if created else "Updated"
+        self.stdout.write(f"{action} superuser '{username}'.")
