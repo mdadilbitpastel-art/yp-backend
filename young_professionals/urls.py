0ddaaf2378b2de90da5c13e784dad25e66e94952
adilsqe13 <adilsqe13@gmail.com>
2026-05-04 16:55:41 +0530

    Fix image issue

--- young_professionals/urls.py
+++ young_professionals/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("api/home/", include("home.urls")),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
